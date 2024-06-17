65ad366c3059c9e6621d1a1d57157624aa177324
lunafl1are <lunafl1are@protonmail.com>
2024-06-17 18:52:25 +0300

    Update config settings

---
+++ individual-hw/task-4-5/src/ObjectPool/ReusableBullet.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Threading;
+
+namespace task_4_5.ObjectPool
+{
+    public class ReusableBullet
+    {
+        private static int _availableId = 1;
+        private int _id;
+        private Pool<ReusableBullet> _nativePool;
+        
+        
+        public ReusableBullet(Pool<ReusableBullet> nativePool)
+        {
+            _id = _availableId;
+            _availableId++;
+
+            _nativePool = nativePool;
+            Console.WriteLine($"Bullet#{_id} was created");
+        }
+
+        public void Launch()
+        {
+            Console.WriteLine($"Bullet#{_id} was launched");
+        }
+
+        public void Reuse()
+        {
+            _nativePool.AddReusableObject(this);
+            
+            Console.WriteLine($"Bullet#{_id} was reused");
+        }
+    }
+}
