f43b3a7245137da26b3e9d04888519a09d31ef38
lunafl1are <lunafl1are@protonmail.com>
2024-06-18 18:52:25 +0300

    improve logging

---
+++ individual-hw/task-4-5/src/Mediator/MediatorComponent.cs
@@ -0,0 +1,12 @@
+﻿namespace task_4_5.Mediator
+{
+    public class MediatorComponent
+    {
+        public Mediator Mediator { protected get; set; }
+
+        public MediatorComponent(Mediator mediator = null)
+        {
+            Mediator = mediator;
+        }
+    }
+}
