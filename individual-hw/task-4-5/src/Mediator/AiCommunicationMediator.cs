2218dd5bb1a8980d5eb6ef5b54927d2f1877ca6b
lunafl1are <lunafl1are@protonmail.com>
2024-06-18 18:52:25 +0300

    update npm packages

---
+++ individual-hw/task-4-5/src/Mediator/AiCommunicationMediator.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace task_4_5.Mediator
+{
+    public class AiCommunicationMediator : Mediator
+    {
+        private AiCommunicationComponent _aiCommunicationComponent1;
+        private AiCommunicationComponent _aiCommunicationComponent2;
+
+        public AiCommunicationMediator(AiCommunicationComponent aiCommunicationComponent1, AiCommunicationComponent aiCommunicationComponent2)
+        {
+            _aiCommunicationComponent1 = aiCommunicationComponent1;
+            _aiCommunicationComponent2 = aiCommunicationComponent2;
+
+            _aiCommunicationComponent1.Mediator = this;
+            _aiCommunicationComponent2.Mediator = this;
+        }
+
+        public void PassMessage(MediatorComponent mediatorComponent, MessageType messageType)
+        {
+            var aiToNotify = mediatorComponent == _aiCommunicationComponent1
+                ? _aiCommunicationComponent2
+                : _aiCommunicationComponent1;
+            
+            Console.WriteLine($"AI#{aiToNotify.Id} receives message {messageType}");
+            
+            switch (messageType)
+            {
+                case MessageType.EnemySpotted:
+                    aiToNotify.AttackEnemy();
+                    break;
+                case MessageType.EnemyLost:
+                    aiToNotify.SearchForEnemy();
+                    break;
+            }
+        }
+    }
+}
