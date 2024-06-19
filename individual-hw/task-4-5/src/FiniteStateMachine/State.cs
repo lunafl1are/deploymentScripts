516421fdcbefd10a11da277fca25e29f4e3236d3
lunafl1are <lunafl1are@protonmail.com>
2024-06-19 18:52:25 +0300

    fixed api integration

---
+++ individual-hw/task-4-5/src/FiniteStateMachine/State.cs
@@ -0,0 +1,9 @@
+﻿namespace task_4_5.FiniteStateMachine
+{
+    public abstract class State
+    {
+        public abstract State HandleOnEnemySpotted();
+        public abstract State HandleOnDamageTaken();
+        public abstract State HandleOnEnemyLost();
+    }
+}
