c159fc3e74b987336fdc999c0228462e4c747eff
lunafl1are <lunafl1are@protonmail.com>
2024-06-19 18:52:25 +0300

    add new icons

---
+++ individual-hw/task-4-5/src/Mediator/AiCommunicationComponent.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace task_4_5.Mediator
+{
+    public class AiCommunicationComponent : MediatorComponent
+    {
+        private static int _availableId = 1;
+        public int Id { get; private set; }
+
+        public AiCommunicationComponent(Mediator mediator = null) : base(mediator)
+        {
+            Id = _availableId;
+            _availableId++;
+        }
+
+        public void SpotEnemy()
+        {
+            Console.WriteLine($"AI#{Id} tells that enemy was spotted");
+            Mediator.PassMessage(this, MessageType.EnemySpotted);
+        }
+
+        public void LooseEnemy()
+        {
+            Console.WriteLine($"AI#{Id} tells that enemy was lost");
+            Mediator.PassMessage(this, MessageType.EnemyLost);
+        }
+
+        public void SearchForEnemy()
+        {
+            Console.WriteLine($"AI#{Id} is searching for enemy");
+        }
+
+        public void AttackEnemy()
+        {
+            Console.WriteLine($"AI#{Id} is attacking enemy");
+        }
+    }
+}
