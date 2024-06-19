7a8da388a953b3dc79d5bc184bff8dccf4c585db
lunafl1are <lunafl1are@protonmail.com>
2024-06-19 18:52:25 +0300

    Fix bug in login form

---
+++ individual-hw/task-4-5/src/FiniteStateMachine/SearchingState.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace task_4_5.FiniteStateMachine
+{
+    public class SearchingState : State
+    {
+        public SearchingState()
+        {
+            Console.WriteLine("AI is searching (SearchingState)");
+        }
+
+        public override State HandleOnEnemySpotted()
+        {
+            return new AttackingState();
+        }
+
+        public override State HandleOnDamageTaken()
+        {
+            return new AttackingState();
+        }
+
+        public override State HandleOnEnemyLost()
+        {
+            return new IdleState();
+        }
+    }
+}
