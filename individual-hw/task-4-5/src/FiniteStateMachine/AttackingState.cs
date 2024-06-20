23a5d0b8ac4943c05ab1f2a5de132466f1cf3231
lunafl1are <lunafl1are@protonmail.com>
2024-06-20 18:52:25 +0300

    update environment variables

---
+++ individual-hw/task-4-5/src/FiniteStateMachine/AttackingState.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace task_4_5.FiniteStateMachine
+{
+    public class AttackingState : State
+    {
+        public AttackingState()
+        {
+            Console.WriteLine("AI is attacking (AttackingState)");
+        }
+
+        public override State HandleOnEnemySpotted()
+        {
+            return this;
+        }
+
+        public override State HandleOnDamageTaken()
+        {
+            return this;
+        }
+
+        public override State HandleOnEnemyLost()
+        {
+            return new SearchingState();
+        }
+    }
+}
