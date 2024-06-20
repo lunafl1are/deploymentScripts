3b2d5f8f8d15051627badd3053f6d8abd87d69d9
lunafl1are <lunafl1are@protonmail.com>
2024-06-20 18:52:25 +0300

    update server config

---
+++ individual-hw/task-4-5/src/FiniteStateMachine/IdleState.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace task_4_5.FiniteStateMachine
+{
+    public class IdleState : State
+    {
+        public IdleState()
+        {
+            Console.WriteLine("AI is patrolling (IdleState)");
+        }
+
+        public override State HandleOnEnemySpotted()
+        {
+            return new AttackingState();
+        }
+
+        public override State HandleOnDamageTaken()
+        {
+            return new AttackingState();
+        }
+
+        public override State HandleOnEnemyLost()
+        {
+            return this;
+        }
+    }
+}
