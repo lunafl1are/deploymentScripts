706384d8a226c53e5ceeda586dbabe215d7397d4
lunafl1are <lunafl1are@protonmail.com>
2024-06-20 18:52:25 +0300

    add new utilities

---
+++ individual-hw/task-4-5/src/FiniteStateMachine/AiContext.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace task_4_5.FiniteStateMachine
+{
+    public class AiContext
+    {
+        private State _currentState;
+
+        public AiContext(State _initialState)
+        {
+            _currentState = _initialState;
+        }
+
+        public void SpotEnemy()
+        {
+            Console.WriteLine("Enemy was spotted");
+            _currentState = _currentState.HandleOnEnemySpotted();
+        }
+
+        public void TakeDamage()
+        {
+            Console.WriteLine("Damage was taken");
+            _currentState = _currentState.HandleOnDamageTaken();
+        }
+
+        public void LostEnemy()
+        {
+            Console.WriteLine("Enemy was lost");
+            _currentState = _currentState.HandleOnEnemyLost();
+        }
+    }
+}
